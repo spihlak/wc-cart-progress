1d4228fd313fc4df516f0a9a4e43d7c01d9ae4be
Siim Pihlak <siimpihlak@Siims-Mac-mini.local>
2024-12-06 13:39:20 +0200

    Rendering the code in Admin

--- includes/class-wc-cart-progress-settings.php
+++ includes/class-wc-cart-progress-settings.php
@@ -23,28 +23,45 @@ class WC_Cart_Progress_Settings {
         );
     }
 
+
+
     public function register_settings() {
         register_setting('wc_cart_progress_settings_group', 'wc_cart_progress_settings', [$this, 'sanitize_settings']);
         add_settings_section('wc_cart_progress_general_section', '', '', 'wc-cart-progress-settings');
         add_settings_field('cart_steps', __('Cart Progress Steps', 'wc-cart-progress'), [$this, 'render_steps_field'], 'wc-cart-progress-settings', 'wc_cart_progress_general_section');
     }
 
-    public function render_steps_field() {
-        $options = get_option('wc_cart_progress_settings');
-        $steps = isset($options['steps']) ? $options['steps'] : [];
-
+    public function render_settings_page() {
         ?>
-        <div id="steps-wrapper">
-            <?php foreach ($steps as $index => $step): ?>
-                <div class="step-row">
-                    <label><?php echo __('Step ' . ($index + 1), 'wc-cart-progress'); ?></label>
-                    <input type="number" name="wc_cart_progress_settings[steps][<?php echo $index; ?>][threshold]" value="<?php echo esc_attr($step['threshold'] ?? ''); ?>" step="0.01" min="0" /> €
-                    <input type="text" name="wc_cart_progress_settings[steps][<?php echo $index; ?>][label]" value="<?php echo esc_attr($step['label'] ?? ''); ?>" placeholder="Step Label" />
-                    <button type="button" class="remove-step">Remove</button>
+        <div class="wrap">
+            <h1><?php _e('WC Cart Progress Settings', 'wc-cart-progress'); ?></h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields('wc_cart_progress_settings_group');
+                do_settings_sections('wc-cart-progress-settings');
+                ?>
+                <div id="steps-wrapper">
+                    <?php
+                    $options = get_option('wc_cart_progress_settings');
+                    $steps = isset($options['steps']) ? $options['steps'] : [];
+                    if (empty($steps)) {
+                        // Optionally, display a default step to kick things off
+                        $steps[] = ['threshold' => 59, 'label' => 'Free shipping'];
+                    }
+
+                    foreach ($steps as $index => $step): ?>
+                        <div class="step-row">
+                            <label><?php echo __('Step ' . ($index + 1), 'wc-cart-progress'); ?></label>
+                            <input type="number" name="wc_cart_progress_settings[steps][<?php echo $index; ?>][threshold]" value="<?php echo esc_attr($step['threshold']); ?>" step="0.01" min="0" /> €
+                            <input type="text" name="wc_cart_progress_settings[steps][<?php echo $index; ?>][label]" value="<?php echo esc_attr($step['label']); ?>" placeholder="Step Label" />
+                            <button type="button" class="remove-step">Remove</button>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
-            <?php endforeach; ?>
+                <button type="button" id="add-step">Add Step</button>
+                <?php submit_button(); ?>
+            </form>
         </div>
-        <button type="button" id="add-step">Add Step</button>
         <?php
     }
 
