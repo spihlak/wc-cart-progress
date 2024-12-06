fec7834411c96db07f2b1d53db6c47b2499b7826
Siim Pihlak <siimpihlak@Siims-Mac-mini.local>
2024-12-06 13:07:57 +0200

    Refactoring, Dynamic code

--- wc-cart-progress.php
+++ wc-cart-progress.php
@@ -13,12 +13,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once plugin_dir_path(__FILE__) . 'includes/class-wc-cart-progress-settings.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-wc-cart-progress-bar.php';
-require_once plugin_dir_path(__FILE__) . 'includes/class-wc-cart-progress-cart-admin.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-wc-cart-progress-admin.php';
 
 function wc_cart_progress_init() {
     new WC_Cart_Progress_Settings();
     new WC_Cart_Progress_Bar();
-    new WC_Cart_Progress_Cart_Admin();
+    new WC_Cart_Progress_Admin();
 }
 
 add_action('plugins_loaded', 'wc_cart_progress_init');
