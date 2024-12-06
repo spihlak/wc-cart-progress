356648b535e024f00a1d0426a075118b853f2e6e
Siim Pihlak <siimpihlak@Siims-Mac-mini.local>
2024-12-06 11:51:34 +0200

    Testing code injection

--- templates/wc-cart-progress-bar.php
+++ templates/wc-cart-progress-bar.php
@@ -1,32 +1,5 @@
 <?php
 
-?>
-<div class="wc-cart-progress-bar">
-    <div class="wc-cart-progress-bar-inner">
+    echo __('Test', 'wc-cart-progress');
 
-        <div class="wc-cart-progress-bar-items-wrapper">
-            <div class="wc-cart-progress-bar-item">              
-            </div>
-        </div>
-
-        <div class="wc-cart-progress-bar-fill-wrapper">
-            <div class="wc-cart-progress-bar-fill-inner">
-                <div class="wc-cart-progress-bar-fill"></div>
-            </div>
-        </div>
-
-        <div class="wc-cart-progress-bar-done-marker-wrapper">
-            <div class="wc-cart-progress-bar-done-marker">
-                <i class="fa-solid fa-check"></i>
-            </div>
-        </div>
-
-        <div class="wc-cart-progress-bar-content-wrapper">
-            <div class="wc-cart-progress-bar-content">
-                <?php _e( 'Your shopping cart is empty.', 'wc-cart-progress' ); ?>
-            </div>
-        </div>
-    </div>
-</div>
-<?php
 ?>
