b2e7f491e1efdcc16246e6aa5445a8715d90401d
Siim Pihlak <siimpihlak@Siims-Mac-mini.local>
2024-12-10 14:16:22 +0200

    Admin. Added Image URL field

--- includes/class-wc-cart-progress-settings.php
+++ includes/class-wc-cart-progress-settings.php
@@ -69,7 +69,7 @@ class WC_Cart_Progress_Settings {
                             <td><input type="number" style="width:100%;" name="wc_cart_progress_settings[steps][<?php echo $index; ?>][threshold]" value="<?php echo esc_attr($step['threshold']); ?>" step="0.01" min="0" /></td>
                             <td><input type="text" style="width:100%;" name="wc_cart_progress_settings[steps][<?php echo $index; ?>][label]" value="<?php echo esc_attr($step['label']); ?>" placeholder="Step Label" /></td>
                             <td><input type="text" style="width:100%;" name="wc_cart_progress_settings[steps][<?php echo $index; ?>][image_url]" value="<?php echo esc_attr($step['image_url']); ?>" placeholder="Image URL" /></td>
-                            <td align="left"><button type="button" class="remove-step"><span class="dashicons dashicons-no-alt"></span></button></td>
+                            <td align="right"><button type="button" class="remove-step"><span class="dashicons dashicons-no-alt"></span></button></td>
                         </tr>
                     <?php endforeach; ?>
                     </tbody>
