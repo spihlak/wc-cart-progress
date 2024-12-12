6c3d7416445db290a004172138060ae899b8662e
Siim Pihlak <siimpihlak@Siims-Mac-mini.local>
2024-12-12 15:32:05 +0200

    Frontend Cart Update

--- includes/class-wc-cart-progress-bar.php
+++ includes/class-wc-cart-progress-bar.php
@@ -121,7 +121,7 @@ class WC_Cart_Progress_Bar {
                     setTimeout(function() {
                         progressBar = initializeCartProgress();
                         $container.css('opacity', '1');
-                    }, 300);
+                    }, 500);
                 });
             } else {
                 // Mini-cart handlers
